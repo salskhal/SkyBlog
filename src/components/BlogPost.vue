3c7b3a8758dc2be74c7de649c2e44a703f781ab4
salskhal <olaniyisal@gmail.com>
2022-08-13 12:22:30 +0100

    feat/ added the blog section on landing page

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,19 +1,50 @@
 <template>
-  <div>
-    <img src="../assets/blogCards/stock-1.jpg" alt="" v-show="blog.blogCoverPhoto === 'stock-1'">
-    <img src="../assets/blogCards/stock-2.jpg" alt="" v-show="blog.blogCoverPhoto === 'stock-2'">
-    <img src="../assets/blogCards/stock-3.jpg" alt="" v-show="blog.blogCoverPhoto === 'stock-3'">
-    <img src="../assets/blogCards/stock-4.jpg" alt="" v-show="blog.blogCoverPhoto === 'stock-4'">
-    <p>{{blog.blogCoverPhoto}}</p>
+  <div class="relative cursor-pointer bg-white rounded-lg overflow-hidden flex flex-col">
+    <div class="absolute gap-2 flex right-3 top-3">
+      <div class="h-8 w-8 bg-white flex p-2 border rounded-full items-center justify-center hover:text-white hover:bg-red-600">
+        <Delete  />
+      </div>
+      <div class="h-8 w-8 bg-white flex p-2  border rounded-full items-center justify-center hover:text-white hover:bg-black">
+        <Edit/>
+      </div>
+    </div>
+    <img
+      src="../assets/blogCards/stock-1.jpg"
+      alt=""
+      v-show="blog.blogCoverPhoto === 'stock-1'"
+    />
+    <img
+      src="../assets/blogCards/stock-2.jpg"
+      alt=""
+      v-show="blog.blogCoverPhoto === 'stock-2'"
+    />
+    <img
+      src="../assets/blogCards/stock-3.jpg"
+      alt=""
+      v-show="blog.blogCoverPhoto === 'stock-3'"
+    />
+    <img
+      src="../assets/blogCards/stock-4.jpg"
+      alt=""
+      v-show="blog.blogCoverPhoto === 'stock-4'"
+    />
+    <div class="p-8">
+        <h4>{{blog.blogTitle}}</h4>
+        <h6>Posted on: {{blog.blogDate}}</h6>
+        <router-link to="#" class="flex items-center gap-2 hover:text-gray-600 ease-in-out duration-500 mt-5"> 
+            View the post <Arrow class="w-3" />
+        </router-link>
+    </div>
   </div>
 </template>
 
 <script setup>
-    defineProps({
-        blog: Object
-    })
+import Delete from "../assets/Icons/trash-regular.svg";
+import Edit from "../assets/Icons/edit-regular.svg";
+import Arrow from "../assets/Icons/arrow-right-light.svg"
+defineProps({
+  blog: Object,
+});
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <Landing :title="landingInfo" />
-    <div class="grid md:grid-cols-2 lg:px-32 px-16 gap-6 py-10">
+    <div class="grid md:grid-cols-2 lg:px-32 px-16 gap-6 py-10 mb-14">
       <BlogCard
         v-for="(blog, index) in sampleBlog"
         :content="blog"
@@ -9,11 +9,11 @@
         :img="blog.img"
       />
     </div>
-    <div class="px-12 py-12 flex flex-col items-center">
+    <div class=" px-28 md:px-14 lg:px-24 py-10 lg:py-28 flex flex-col  bg-[#e9ca96]">
       <div>
-        <h3>View More Recent Blogs</h3>
+        <h3 class="mb-4 text-2xl text-gray-800">View More Recent Blogs</h3>
       </div>
-      <div class="grid  md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div class="grid  md:grid-cols-2 lg:grid-cols-4 gap-8">
         <BlogPost
           v-for="(blog, index) in blogCards"
           :key="index"
