3d47cce54a30aa8547ca6a1b665b1a8947a0eb23
salskhal <olaniyisal@gmail.com>
2022-08-15 19:57:53 +0100

    feat/ added register and forgot password page style

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,11 +1,40 @@
 <template>
-  <h1>forgot password page</h1>
+  <div
+    class="o overflow-hidden flex h-screen justify-center self-center my-0 w-full mx-auto"
+  >
+    <form
+      class="login py-0 px-3 relative flex flex-col justify-center items-center md:px-12"
+    >
+    <h2 class="text-center text-2xl mb-8 text-mobileBg lg:text-3xl font-semibold">
+        Reset Password
+      </h2>
+      <p class="login-register mb-8">
+       Forgot your passowrd? Enter your email to reset it
+      </p>
+      
+      <div class="inputs w-full max-w-xs">
+        <div class="input relative flex justify-center items-center mb-2">
+          <input
+            type="text"
+            placeholder="Email..."
+            v-model="email"
+            class="w-full border-none bg-input p-1 pl-7 h-12 focus:outline-none"
+          />
+          <Email class="icon w-3 absolute left-2"/>
+        </div>
+      </div>
+      
+      <button class="bg-primary py-2 px-4 rounded-full mt-3">Reset</button>
+    </form>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
 
-}
+import { ref } from "@vue/reactivity";
+import Email from "../assets/Icons/envelope-regular.svg";
+
+const email = ref("")
 </script>
 
 <style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,85 @@
 <template>
-  <h1>Register page</h1>
+  <div
+    class="o overflow-hidden flex h-screen justify-center self-center my-0 w-full mx-auto"
+  >
+    <form
+      class="login py-0 px-3 relative flex flex-col justify-center items-center md:px-12"
+    >
+      <p class="login-register mb-8">
+        Already have an account
+        <router-link :to="{ name: 'Login' }" class="text-primary"
+          >Log In</router-link
+        >
+      </p>
+      <h2 class="text-center text-2xl mb-8 text-mobileBg lg:text-3xl font-semibold">
+        Register 
+      </h2>
+      <div class="inputs w-full max-w-xs">
+        <div class="input relative flex justify-center items-center mb-2">
+          <input
+            type="text"
+            placeholder="First Name"
+            v-model="firstName"
+            class="w-full border-none bg-input p-1 pl-7 h-12 focus:outline-none"
+          />
+          <User class="icon w-3 absolute left-2"/>
+        </div>
+        <div class="input relative flex justify-center items-center mb-2">
+          <input
+            type="text"
+            placeholder="Last Name"
+            v-model="lastName"
+            class="w-full border-none bg-input p-1 pl-7 h-12 focus:outline-none"
+          />
+          <User class="icon w-3 absolute left-2"/>
+        </div>
+        <div class="input relative flex justify-center items-center mb-2">
+          <input
+            type="text"
+            placeholder="Username"
+            v-model="username"
+            class="w-full border-none bg-input p-1 pl-7 h-12 focus:outline-none"
+          />
+          <User class="icon w-3 absolute left-2"/>
+        </div>
+        <div class="input relative flex justify-center items-center mb-2">
+          <input
+            type="text"
+            placeholder="Email..."
+            v-model="email"
+            class="w-full border-none bg-input p-1 pl-7 h-12 focus:outline-none"
+          />
+          <Email class="icon w-3 absolute left-2"/>
+        </div>
+        <div class="input relative flex justify-center items-center mb-2">
+          <input
+            type="password"
+            placeholder="Password"
+            v-model="password"
+            class="w-full border-none bg-input p-1 pl-7 h-12 focus:outline-none"
+          />
+          <Password class="icon w-3 absolute left-2"/>
+        </div>
+      </div>
+      
+      <button class="bg-primary py-2 px-4 rounded-full mt-4">Register</button>
+    </form>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
 
-}
+import { ref } from "@vue/reactivity";
+import Email from "../assets/Icons/envelope-regular.svg";
+import Password from "../assets/Icons/lock-alt-solid.svg";
+import User from "../assets/Icons/user-alt-light.svg"
+
+const email = ref("");
+const password = ref("");
+const firstName = ref("")
+const lastName = ref("")
+const username = ref("")
+
 </script>
 
 <style>
